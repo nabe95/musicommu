d847772e580f48fb8d25ac614659ab18ec5324ef
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-11-07 06:55:27 +0000

    [add] bootstrap devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  #ユーザー用
+  # URL /users/sign_in ...
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admins, skip: [:registrations, :passwwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
