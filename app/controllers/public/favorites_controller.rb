b3885ee5230d452e0b0070b152df919b358296ac
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-11-11 05:28:40 +0000

    [add]いいね非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,18 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    post_favorite = current_user.favorites.new(post_id: post.id)
-    post_favorite.save
-    redirect_to request.referer
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.new(post_id: @post.id)
+    @favorite.save
+    #非同期通信:js.erbファイルへ
+    render 'replace_favorite'
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
-    favorite.destroy
-    redirect_to request.referer
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.find_by(post_id: @post.id)
+    @favorite.destroy
+    #非同期通信:js.erbファイルへ
+    render 'replace_favorite'
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,13 @@ class Post < ApplicationRecord
   has_one_attached :image
   #コメント機能
   has_many :post_comments, dependent: :destroy
-  #いいね
+  #いいね機能
   has_many :favorites, dependent: :destroy
   
   validates :title,presence:true
   validates :body,presence:true
   
+  #写真の登録がない場合のイメージ画像を
   def get_image
     if image.attached?
       image
@@ -18,6 +19,7 @@ class Post < ApplicationRecord
     end
   end
   
+  #引数で渡されたpost_idがFavoritesテーブル内に存在するか確認
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
