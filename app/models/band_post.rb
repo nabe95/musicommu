d900e2b2382e381a68208c5d7034dc83db43ecb1
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-11-15 06:16:13 +0000

    [add]バンド募集公開非公開機能

--- app/models/band_post.rb
+++ app/models/band_post.rb
@@ -8,8 +8,24 @@ class BandPost < ApplicationRecord
   validates :instrument,presence:true
   validates :genre,presence:true
 
+  # 活動日時(指定なし、平日、休日)
   enum activity: { unspecified: 0, weekdays: 1, holiday: 2 }
+  # 活動の方針
   enum direction: { hobby:0, independent:1, pro: 2 }
+  # バンドでやる楽曲の種類(指定なし、コピー、オリジナル)
   enum band_type: { notype: 0, copy: 1, original: 2 }
+  # 投稿の公開と非公開
+  enum status: { public: 0, private:1 }, _prefix: true
+  
+  def status_label
+    case status.to_sym
+    when :public
+      '<span class="text-success">'.html_safe + I18n.t("enums.band_post.status.public") + '</span>'.html_safe
+    when :private
+      '<span class="text-danger">'.html_safe + I18n.t("enums.band_post.status.private") + '</span>'.html_safe
+    else
+      status.to_s
+    end
+  end
 
 end
