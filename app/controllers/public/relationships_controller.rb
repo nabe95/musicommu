8174ef1f07741a625a8eda732e90a34c8b404a32
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-12-16 06:47:32 +0000

    フォロー部分テンプレート

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,11 +3,15 @@ class Public::RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
+    #非同期通信:js.erbファイルへ
+    render 'replace_relationship'
   end
 
   #フォローを外す
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
+    #非同期通信:js.erbファイルへ
+    render 'replace_relationship'
   end
 end
